a582e2215bd8603dbd2c4d68663a0c8517ebe13d
Pierre Bretéché <pbreteche@dawan.fr>
2021-11-24 16:28:31 +0100

    My first controller action

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends AbstractController
+{
+
+    public function index(): Response
+    {
+        return new Response('Ma première réponse');
+    }
+}
