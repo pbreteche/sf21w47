35849d98c7772c54f572be6e459420b29a912a0e
Pierre Bretéché <pbreteche@dawan.fr>
2021-11-26 10:11:04 +0100

    Better homepage

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -9,12 +9,14 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PostController extends AbstractController
 {
+    const MAX_POSTS_PER_PAGE = 10;
+
     /**
      * @Route("/")
      */
     public function homepage(PostRepository $repository): Response
     {
-        $posts = $repository->findAll();
+        $posts = $repository->findBy([], ['createdAt' => 'DESC'], self::MAX_POSTS_PER_PAGE);
 
         return $this->render('post/index.html.twig', [
             'title' => 'Bienvenue sur mon blog!',
