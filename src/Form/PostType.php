9d5b011c0f1ddef16870c906c1cc9bc260329df4
Pierre Bretéché <pbreteche@dawan.fr>
2021-11-29 16:16:19 +0100

    use post author in detail, create and edit pages

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -2,7 +2,9 @@
 
 namespace App\Form;
 
+use App\Entity\Author;
 use App\Entity\Post;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -20,6 +22,11 @@ class PostType extends AbstractType
             ->add('body', TextareaType::class, [
                 'attr' => [ 'cols' => 60, 'rows' => 10 ],
                 'help' => 'Écrivez un contenu suffisamment long (10).',
+            ])
+            ->add('writtenBy', EntityType::class, [
+                'class' => Author::class,
+                'choice_label' => 'name',
+                'placeholder' => 'Sélectionnez un auteur.'
             ])
         ;
     }
