04a528427a2d88a1b38249abba72a2e5c35b806f
Pierre Bretéché <pbreteche@dawan.fr>
2021-12-01 11:13:58 +0100

    Create admin section

--- src/Controller/Admin/AuthorController.php
+++ src/Controller/Admin/AuthorController.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Admin;
 
 use App\Entity\Author;
 use App\Form\AuthorType;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,6 +13,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @Route("/accounts")
+ * @IsGranted("ROLE_ADMIN")
  */
 class AuthorController extends AbstractController
 {
