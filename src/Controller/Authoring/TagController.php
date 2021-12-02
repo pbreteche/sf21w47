90ee3e3896ef129eedfee4a8d3773f739699ab1d
Pierre Bretéché <pbreteche@dawan.fr>
2021-12-02 14:24:20 +0100

    create new tag form page

--- src/Controller/Authoring/TagController.php
+++ src/Controller/Authoring/TagController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller\Authoring;
 
+use App\Entity\Tag;
+use App\Form\TagType;
 use App\Repository\TagRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -24,4 +28,28 @@ class TagController extends AbstractController
         ]);
     }
 
+    /**
+     * @Route("/new", methods={"GET", "POST"})
+     */
+    public function create(
+        Request $request,
+        EntityManagerInterface $manager
+    ): Response {
+        $tag = new Tag();
+
+        $form = $this->createForm(TagType::class, $tag);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($tag);
+            $manager->flush();
+            $this->addFlash('notice', 'Votre tag a bien été enregistrée');
+
+            return $this->redirectToRoute('app_authoring_tag_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('authoring/tag/create.html.twig', [
+            'create_form' => $form,
+        ]);
+    }
 }
