4eb62c8e948035fab38a708bd91125d2abbc1b61
Pierre Bretéché <pbreteche@dawan.fr>
2021-12-10 14:55:13 +0100

    locale form set locale in session

---
+++ src/Controller/LocaleController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\LocaleType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class LocaleController extends AbstractController
+{
+    public function choiceList(Request $request): Response
+    {
+        $form = $this->createLocaleForm($request);
+
+        return $this->renderForm('locale/select.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/locale", methods="POST")
+     */
+    public function select(
+        Request $request,
+        TranslatorInterface $translator
+    ): Response {
+        $form = $this->createLocaleForm($request);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $request->getSession()->set('locale', $form->getData());
+            $this->addFlash('notice', $translator->trans('site.locale.flash.change'));
+        }
+
+        return $this->redirectToRoute('app_post_homepage');
+    }
+
+    private function createLocaleForm(Request $request): FormInterface
+    {
+        return $this->createForm(LocaleType::class, $request->getLocale(), [
+            'choices' => [
+                'English' => 'en',
+                'French' => 'fr',
+            ],
+            'choice_loader' => null,
+            'choice_translation_domain' => true,
+        ]);
+    }
+}
